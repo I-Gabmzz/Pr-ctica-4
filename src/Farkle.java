d24cfb8aada04e37663f1e90b19f261678c2dccf
PC OSTRICH <a1197503@uabc.edu.mx>
2025-03-30 17:15:27 -0700

    up

--- src/Farkle.java
+++ src/Farkle.java
@@ -1,7 +1,33 @@
 package src;
 
-public class Farkle {
-    public void dibujar() {
+import javax.swing.*;
 
+public class Farkle {
+    private int opcion;
+    private int jugadores;
+
+    public Farkle()
+    {
+        menuInicial();
+        if (opcion == 0) {
+            iniciarJuego();
+        } else if (opcion == 1) {
+            mostrarCreditos();
+        } else if (opcion == 2) {
+            System.exit(0);
+        }
+    }
+
+    private void iniciarJuego(int jugadores, int puntos) {
+
+    }
+
+    private void mostrarCreditos() {
+    }
+
+    private void menuInicial() {
+        Object[] botones = {"Jugar", "Creditos", "Salir"};
+        int opcion = JOptionPane.showOptionDialog(null, "Bienvenido al juego Farkle, Seleccione una opcion:", "Juego Farkle",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, botones, botones[0]);
     }
 }
