3b0e2e98e8815c54e2ff0cb17cbbcbaea2365a85
PC OSTRICH <a1197503@uabc.edu.mx>
2025-03-30 19:22:46 -0700

    up

--- src/Jugador.java
+++ src/Jugador.java
@@ -1,5 +1,6 @@
 package src;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -7,8 +8,8 @@ public class Jugador {
     private int puntuacionTotal;
     private int puntuacion;
     private ArrayList<Dado> dadosTirados;
-    private ArrayList<Dado> dadosTomados;
-    private int dadosDisponibles;
+    private ArrayList<Integer> dadosTomados;
+    private int dadosDisponibles = 6;
 
 
     public Jugador() {
@@ -23,6 +24,7 @@ public class Jugador {
         int delta = 150;
         dadosTirados.clear();
         for (int i = 0; i < dadosDisponibles; i++) {
+            int xVariable = 975 - dadosDisponibles * delta;
             int xPosicion = 75 + i *  delta ;
             int yPosicion = 450 ;
             Dado dado = new Dado();
@@ -33,6 +35,30 @@ public class Jugador {
         }
     }
 
+    public void guardarDadosTirados() {
+        while (true) {
+            Object[] botones = new Object[dadosTirados.size() + 1];
+            for (int i = 0; i < dadosTirados.size(); i++) {
+                botones[i] = dadosTirados.get(i).getValor();
+            }
+            botones[dadosTirados.size()] = "Continuar";
+
+            int opcion = JOptionPane.showOptionDialog( null, "Estos son los dados tirados, seleccione los que desea guardar en el banco:", "Tomar Dados",
+                    JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, botones, botones[0]);
+            if (opcion == dadosTirados.size()) {
+                break;
+            }
+
+            if (opcion >= 0 && opcion < dadosTirados.size()) {
+                Dado dadoSeleccionado = dadosTirados.remove(opcion);
+                int valorSeleccionado = dadoSeleccionado.getValor();
+                dadosTomados.add(valorSeleccionado);
+                dadosDisponibles--;
+
+                JOptionPane.showMessageDialog(null, "Tomaste el dado con valor: " + valorSeleccionado);
+            }
+        }
+    }
 
     public void mostrarDadosTirados() {
         for (Dado dado : dadosTirados) {
@@ -41,8 +67,8 @@ public class Jugador {
     }
 
     public void mostrarDadosTomados() {
-        for (Dado dado : dadosTomados) {
-            System.out.println(dado.getValor());
+        for (int valor : dadosTomados) {
+            System.out.println(dadosTomados);
         }
     }
 
--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,11 @@ package src;
 public class Main {
     public static void main(String[] args) {
         // Dado prueba = new Dado();
-        //Farkle farkle = new Farkle();
-        //farkle.menuInicial();
+        Farkle farkle = new Farkle();
+        farkle.menuInicial();
         // prueba.lanzarDado();
         Jugador j1 = new Jugador();
         j1.tirarDados(6);
+        j1.guardarDadosTirados();
     }
 }
